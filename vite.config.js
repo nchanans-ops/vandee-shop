9073894ae0fef668bc7b78406bcb900e5d7508d4
Chananchita Yothaphon <n.chanans@gmail.com>
2026-04-13 09:19:37 +0000

    fix: restore base path to /vandee-shop/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/back-vandee/',
+  base: '/vandee-shop/',
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#f5f6f8',
         display: 'standalone',
         orientation: 'any',
-        start_url: '/back-vandee/',
-        scope: '/back-vandee/',
+        start_url: '/vandee-shop/',
+        scope: '/vandee-shop/',
         icons: [
           {
             src: 'icon-192.png',
